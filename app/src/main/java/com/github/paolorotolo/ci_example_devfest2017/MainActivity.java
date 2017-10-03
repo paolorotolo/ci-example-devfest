0864a98623723144e5aa142da46e104f30718cd9
Paolo Rotolo <rotolopao@gmail.com>
2017-10-03 17:52:01 +0200

    First upload

---
+++ app/src/main/java/com/github/paolorotolo/ci_example_devfest2017/MainActivity.java
@@ -0,0 +1,34 @@
+package com.github.paolorotolo.ci_example_devfest2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        editText = findViewById(R.id.edittext);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText());
+            }
+        });
+    }
+}
